7c6e04b2dd118c5aa8fe562329c6b13cec8a0ea0
Guilherme Araujo Alexandre <guilhermealexandre167@gmail.com>
2022-04-10 11:28:37 -0300

    Initial commit

---
+++ app/src/main/java/com/guilherme/applogin/view/telaPrincipal/TelaPrincipal.kt
@@ -0,0 +1,12 @@
+package com.guilherme.applogin.view.telaPrincipal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.guilherme.applogin.R
+
+class TelaPrincipal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_principal)
+    }
+}
