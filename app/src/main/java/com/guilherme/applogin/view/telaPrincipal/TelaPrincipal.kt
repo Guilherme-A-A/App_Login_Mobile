c6005aa04db6ef86a75091686db7e98c51b85fac
Guilherme Araujo Alexandre <guilhermealexandre167@gmail.com>
2022-04-10 12:29:01 -0300

    Finalizado!!!!

--- app/src/main/java/com/guilherme/applogin/view/telaPrincipal/TelaPrincipal.kt
+++ app/src/main/java/com/guilherme/applogin/view/telaPrincipal/TelaPrincipal.kt
@@ -1,12 +1,27 @@
 package com.guilherme.applogin.view.telaPrincipal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import com.guilherme.applogin.R
+import com.guilherme.applogin.databinding.ActivityFormLoginBinding
+import com.guilherme.applogin.databinding.ActivityTelaPrincipalBinding
 
 class TelaPrincipal : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTelaPrincipalBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tela_principal)
+        binding = ActivityTelaPrincipalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btSair.setOnClickListener{ FormLogin ->
+            FirebaseAuth.getInstance().signOut()
+            val voltarTelaLogin = Intent(this, FormLogin::class.java)
+            startActivity(voltarTelaLogin)
+            finish()
+        }
     }
 }
